ca90f2fb093e2599eda9c022d95cb406ffae8cd2
HDzhangkang <13393211992@163.com>
2025-09-15 13:54:28 +0800

    server | client 的组件

---
+++ src/node/services/node_service/include/node_service/visibility_control.h
@@ -0,0 +1,59 @@
+// Copyright 2016 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// 
+#ifndef NODE_SERVICE__VISIBILITY_CONTROL_H_
+#define NODE_SERVICE__VISIBILITY_CONTROL_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+// This logic was borrowed (then namespaced) from the examples on the gcc wiki:
+//     https://gcc.gnu.org/wiki/Visibility
+
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef __GNUC__
+    #define NODE_SERVICE_EXPORT __attribute__ ((dllexport))
+    #define NODE_SERVICE_IMPORT __attribute__ ((dllimport))
+  #else
+    #define NODE_SERVICE_EXPORT __declspec(dllexport)
+    #define NODE_SERVICE_IMPORT __declspec(dllimport)
+  #endif
+  #ifdef NODE_SERVICE_BUILDING_DLL
+    #define NODE_SERVICE_PUBLIC NODE_SERVICE_EXPORT
+  #else
+    #define NODE_SERVICE_PUBLIC NODE_SERVICE_IMPORT
+  #endif
+  #define NODE_SERVICE_PUBLIC_TYPE NODE_SERVICE_PUBLIC
+  #define NODE_SERVICE_LOCAL
+#else
+  #define NODE_SERVICE_EXPORT __attribute__ ((visibility("default")))
+  #define NODE_SERVICE_IMPORT
+  #if __GNUC__ >= 4
+    #define NODE_SERVICE_PUBLIC __attribute__ ((visibility("default")))
+    #define NODE_SERVICE_LOCAL  __attribute__ ((visibility("hidden")))
+  #else
+    #define NODE_SERVICE_PUBLIC
+    #define NODE_SERVICE_LOCAL
+  #endif
+  #define NODE_SERVICE_PUBLIC_TYPE
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // NODE_SERVICE__VISIBILITY_CONTROL_H_
